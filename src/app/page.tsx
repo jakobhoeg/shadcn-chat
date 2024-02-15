5fe77345107c53582fa13284b97b121e83179b6d
Jakob Hoeg Mørk <jakmoe01@easv365.dk>
2024-02-15 15:32:38 +0100

    fix: mobile overflow issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
 
   return (
-    <main className="flex h-screen flex-col items-center justify-center p-4 md:px-24 py-32 gap-4">
+    <main className="flex h-[calc(100dvh)] flex-col items-center justify-center p-4 md:px-24 py-32 gap-4">
       <div className="flex justify-between max-w-5xl w-full items-center">
         <Link href="#" className="text-4xl font-bold text-gradient">shadcn-chat</Link>
         <Link
