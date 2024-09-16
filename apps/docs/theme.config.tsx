6b1c85f9e40dcdad20c07f8a7761b6c9d862c5c2
Jakob Hoeg <jakmoe01@easv365.dk>
2024-09-16 12:19:09 +0200

    docs: initial docs repo

---
+++ apps/docs/theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>shadcn-chat</span>,
+  project: {
+    link: 'https://github.com/jakobhoeg/shadcn-chat',
+  },
+  docsRepositoryBase: 'https://github.com/jakobhoeg/shadcn-chat',
+  footer: {
+    text: 'shadcn-chat documentation',
+  },
+}
+
+export default config
